d6f09f27441c4ad7c774abcd83d6492c493e975f
Andreas Järvelä <andja235@student.liu.se>
2017-11-03 15:25:15 +0100

    Begin implementation of lab1.2

---
+++ app/src/main/java/com/tddc73/lab1/lab12.java
@@ -0,0 +1,13 @@
+package com.tddc73.lab1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class lab12 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lab12);
+    }
+}
