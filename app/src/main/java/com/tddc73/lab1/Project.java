9eb5d44f571bff826b968f18c1c3d4e61930a876
Andreas Järvelä <andja235@student.liu.se>
2017-11-13 12:04:51 +0100

    Add base files for Project

---
+++ app/src/main/java/com/tddc73/lab1/Project.java
@@ -0,0 +1,13 @@
+package com.tddc73.lab1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Project extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.account_registration_password_strength);
+    }
+}
