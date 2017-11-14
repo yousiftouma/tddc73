45da2d6df845110c38af1865f033b8bdad77c382
Andreas Järvelä <andja235@student.liu.se>
2017-11-14 16:03:25 +0100

    Add javadoc to AccountRegistrationComponent

--- app/src/main/java/com/tddc73/lab1/projectcomponents/AccountRegistrationComponent.java
+++ app/src/main/java/com/tddc73/lab1/projectcomponents/AccountRegistrationComponent.java
@@ -11,7 +11,8 @@ import android.widget.LinearLayout;
 import com.tddc73.lab1.R;
 
 /**
- * Created by Andreas on 2017-11-13.
+ * Component that handles account registration. Visualises if any field has incorrect entries. Can override bool functions to define
+ * custom validations for fields. Set a SuccesfulRegistrationHandler to handle successful registration.
  */
 public class AccountRegistrationComponent extends LinearLayout{
 
@@ -43,6 +44,9 @@ public class AccountRegistrationComponent extends LinearLayout{
         init();
     }
 
+    /***
+     * Initializes all subcomponents for this component. Creates an on-click listener for the account registration button.
+     */
     private void init() {
         inflate(getContext(), R.layout.account_registration, this);
         fullName = (EditText) findViewById(R.id.fullnameET);
@@ -58,6 +62,10 @@ public class AccountRegistrationComponent extends LinearLayout{
         });
     }
 
+    /***
+     * Takes entries of all fields and checks if they are valid. Runs the callback method for the successfulRegistrationHandler if all
+     * fields are correct. Never runs the callback method if no handler is set by the user, result is ignored.
+     */
     private void validateRegistration(){
         boolean successful = true;
         String fullNameText = fullName.getText().toString();
@@ -90,30 +98,57 @@ public class AccountRegistrationComponent extends LinearLayout{
             registrationHandler.onSuccessfulRegistration(fullNameText, usernameText, emailText, passwordText);
     }
 
+    /***
+     * Defines if a Full Name entry is valid.
+     * @param currentEntry
+     * @return true if full name entry is valid
+     */
     public boolean isFullNameCorrect(String currentEntry){
 
         return true;
     }
 
+    /***
+     * Defines if a Username entry is valid.
+     * @param currentEntry
+     * @return true if username entry is valid
+     */
     public boolean isUsernameCorrect(String currentEntry){
 
         return true;
     }
 
+    /***
+     * Defines if a email entry is valid.
+     * @param currentEntry
+     * @return true if email entry is valid
+     */
     public boolean isEmailCorrect(String currentEntry){
 
         return currentEntry.equals("a");
     }
 
+    /***
+     * Sets the algorithm used for calculating password strength.
+     * @param psc
+     */
     public void setPasswordStrengthCalculator(PasswordStrengthCalculator psc) {
         password.setPasswordStrengthCalculator(psc);
     }
 
+    /***
+     * Defines if a password entry is valid, differs from calculating password strength
+     * @param currentEntry
+     * @return true if a password entry is valid
+     */
     public boolean isPasswordOk(String currentEntry) {
         return password.getStrength() != PasswordStrengthMeter.PasswordStrength.INVALID;
     }
 
-
+    /***
+     * Sets a handler for this component, allows user to handle results for a successful registration.
+     * @param registrationHandler
+     */
     public void setRegistrationHandler(SuccessfulRegistrationHandler registrationHandler) {
         this.registrationHandler = registrationHandler;
     }
